e70fcf51c72720f5f92595397b9bca91139f818c
Your Name <matbaugh2003@yahoo.com>
2011-10-03 17:41:08 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MobileLMSRails::Application.config.secret_token = 'd3448d4a5d7fcfa4855cbc951bbf1f33263a4555cf2c03deaa22da2b26f69cba7921e50f5f31ca9de9deb96d945bffbca1f552cc028a60c1a6a9b7274e40d4c8'
